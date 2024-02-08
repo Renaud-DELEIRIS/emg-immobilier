0770c308b414b5d60cd0221f6dd1cbd11f0ed03f
emgdevteam <valentin.corsain@emg-associes.ch>
2024-02-08 18:39:54 +0100

    fix carInfoWrapper name

--- src/components/steps/CarInfo/CarInfoWrapper.tsx
+++ src/components/steps/CarInfo/CarInfoWrapper.tsx
@@ -1,20 +1,26 @@
 import { useTranslation } from "next-i18next";
+import { useState } from "react";
 import { useFormStore } from "~/stores/form";
 import StepContainer from "../StepContainer";
+import CarBrand from "./CarBrand";
 import CarModel from "./CarModel";
 
-const CarBrand = () => {
+const CarInfoWrapper = () => {
   const lead = useFormStore((state) => state.data);
   const changeLead = useFormStore((state) => state.setData);
   const nextStep = useFormStore((state) => state.nextStep);
   const { t } = useTranslation("step");
 
+  const [activeSubStep, setActiveSubStep] = useState<"car_brand" | "car_model">(
+    "car_brand"
+  );
+
   return (
     <StepContainer title={t("car-brand.title")} stepId="car_info">
-      {lead.car_brand == undefined && <CarBrand />}
-      {lead.car_model == undefined && <CarModel />}
+      {activeSubStep == "car_brand" && <CarBrand />}
+      {activeSubStep == "car_model" && <CarModel />}
     </StepContainer>
   );
 };
 
-export default CarBrand;
+export default CarInfoWrapper;
